39863270de48fc9bf4fd363708a6f59b080b07f0
lukaw3d <lukajeran@oasislabs.com>
2022-03-10 03:48:02 +0100

    Check relative links have a file extension, and file exists

--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -24,6 +24,60 @@ const options = {
     'no-inline-html': false,
     'no-alt-text': false,
   },
+  customRules: [{
+    // Check relative links have a file extension, and file exists
+    names: ['relative-links-have-ext'],
+    description: 'for Docusaurus routing',
+    tags: ['links'],
+    information: new URL('https://github.com/oasisprotocol/docs/issues/4'),
+    function: ({ name, tokens }, onError) => {
+      const fs = require('fs');
+      const path = require('path');
+      const dir = path.dirname(name);
+      for (const token of tokens) {
+        if (token.type === 'inline') {
+          for (const child of token.children) {
+            if (child.type === 'link_open') {
+              const [_key, href] = child.attrs.find(([key, value]) => key === 'href');
+              const isAbsoluteUrl = new URL(href, 'relative://a.b').protocol !== 'relative:';
+              const isAbsolutePathOrNetworkPath = href.startsWith('/');
+              const isFragment = href.startsWith('#');
+
+              const isRelativePath = !isAbsoluteUrl && !isAbsolutePathOrNetworkPath && !isFragment;
+              if (isRelativePath) {
+                const relativePath = href.split('#')[0];
+                const missingExtension = !path.extname(relativePath);
+                const missingFile = !fs.existsSync(path.join(dir, relativePath));
+                if (missingExtension) {
+                  const postfixSuggestions = ['.md', '.mdx', 'README.md', 'README.mdx', '/README.md', '/README.mdx', 'index.md', '/index.md'];
+                  const goodPostfix = postfixSuggestions.find(
+                    postfix => fs.existsSync(path.join(dir, relativePath + postfix))
+                  );
+                  const postfixColumn = 1 + child.line.indexOf(href) + relativePath.length;
+                  const canFix = goodPostfix && child.line.indexOf(href) >= 0;
+                  onError({
+                    lineNumber: child.lineNumber,
+                    context: href,
+                    detail: 'Filename extension missing',
+                    fixInfo: !canFix ? undefined : {
+                      insertText: goodPostfix,
+                      editColumn: postfixColumn,
+                    },
+                  });
+                } else if (missingFile) {
+                  onError({
+                    lineNumber: child.lineNumber,
+                    context: href,
+                    detail: 'File missing',
+                  });
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+  }],
 };
 
 module.exports = options;

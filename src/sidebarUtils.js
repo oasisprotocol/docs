f6a11a8b46be567aac34013f722307d540e6ef29
Matevž Jekovec <matevz@oasisprotocol.org>
2022-05-25 11:30:59 +0200

    deps: Bump docusaurus and other core deps to 2.0.0-beta.20

--- src/sidebarUtils.js
+++ src/sidebarUtils.js
@@ -1,11 +1,16 @@
+// @ts-check
 import {useDocsSidebar} from "@docusaurus/theme-common";
 
-// Builds an index of document id => sidebar item.
-// If the item is external link, href is used as document id.
-// For category item with associated document, trailing slashes are trimmed.
-// For category item without associated document, the label is taken as document id.
-function reindex(sidebar) {
-    for (const item of sidebar) {
+/**
+ * Builds an index of document id => sidebar item.
+ * If the item is external link, href is used as document id.
+ * For category item with associated document, trailing slashes are trimmed.
+ * For category item without associated document, the label is taken as document id.
+ *
+ *  @param {import('@docusaurus/plugin-content-docs').PropSidebar} sidebarItems
+ */
+function reindex(sidebarItems) {
+    for (const item of sidebarItems) {
         globalThis.sidebarItemsMap[item.docId ?? (item.href ? item.href.replace(/\/$/g, '') : item.label)] = item;
         if (item.type === 'category') {
             reindex(item.items)
@@ -21,7 +26,7 @@ export function findSidebarItem(docId) {
     }
     if (globalThis.sidebarItemsMap === undefined) {
         globalThis.sidebarItemsMap = {};
-        reindex(sidebar);
+        reindex(sidebar.items);
     }
     if (globalThis.sidebarItemsMap[docId] === undefined) {
         throw new Error('Unexpected: document with id '+docId+' does not exist.');

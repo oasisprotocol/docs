2a8ff0ab2cd193526a34143d01dea1db2be55e8f
Matevž Jekovec <matevz@oasisprotocol.org>
2023-09-25 12:34:07 +0200

    docs: Add new Reference API URLs

--- sidebarDapp.js
+++ sidebarDapp.js
@@ -22,6 +22,16 @@ const sidebars = {
         'dapp/sapphire/gasless',
         'dapp/sapphire/precompiles',
         'dapp/sapphire/addresses',
+        {
+          type: 'link',
+          label: 'TypeScript API',
+          href: 'https://api.docs.oasis.io/js/sapphire-paratime',
+        },
+        {
+          type: 'link',
+          label: 'Solidity API',
+          href: 'https://api.docs.oasis.io/sol/sapphire-contracts',
+        },
       ],
     },
     {
@@ -66,13 +76,8 @@ const sidebars = {
         {
           type: 'link',
           label: 'Rust API',
-          href: 'https://api.docs.oasis.io/oasis-sdk/oasis_contract_sdk',
+          href: 'https://api.docs.oasis.io/rust/oasis_contract_sdk',
         },
-        {
-          type: 'link',
-          label: 'Go API',
-          href: 'https://pkg.go.dev/github.com/oasisprotocol/oasis-sdk/client-sdk/go/client',
-        }
       ],
     },
   ],

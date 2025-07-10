8afffcab2556d95d1a85993d1eacc3c86434b6be
Kaja <kaja.osojnik@gmail.com>
2025-07-10 17:08:54 +0200

    Create CustomDocCardSection component

---
+++ src/components/CustomDocCardSection.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import DocCard from "@theme/DocCard";
+import { findSidebarItem } from "@site/src/sidebarUtils";
+import styles from "./CustomDocCardSection.module.css";
+
+type Props = {
+  title: string;
+  description: string;
+  itemPaths: string[];
+  fullWidth?: boolean;
+  secondDescription?: string;
+  secondItemPaths?: string[];
+  svg?: React.ReactNode;
+};
+
+export default function CustomDocCardSection({ title, description, itemPaths, fullWidth, secondDescription, secondItemPaths, svg }: Props) {
+  const items = itemPaths.map((path) => findSidebarItem(path)).filter(Boolean);
+  const secondItems = secondItemPaths?.map((path) => findSidebarItem(path)).filter(Boolean) ?? [];
+
+  return (
+    <div className={`${styles.customCard} ${fullWidth ? styles.fullWidth : ""}`}>
+      <div className={`${fullWidth && styles.cardContentFlex}`}>
+  {svg && (
+    <div className={styles.animatedSvgWrapper}>
+      {svg}
+    </div>
+  )}
+        <div className={`${fullWidth && styles.halfWidth}`}>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+
+        <div className={`${styles.cardLinksList} ${fullWidth && styles.halfWidth}`}>
+          {items.map((item, idx) => (
+            <div key={idx} className={styles.cardItem} data-custom-doccard>
+              <DocCard item={item} />
+            </div>
+          ))}
+
+          {secondDescription && <p>{secondDescription}</p>}
+
+          {secondItems.length > 0 && (
+            <div className={styles.cardLinksList}>
+              {secondItems.map((item, idx) => (
+                <div key={idx} className={styles.cardItem} data-custom-doccard>
+                  <DocCard item={item} />
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}

50b3636e7c7b0fe08981e74b483f38bd144868cf
Kaja <kaja.osojnik@gmail.com>
2025-07-11 11:29:02 +0200

    Trigger animation on scroll

--- src/components/CustomDocCardSection.tsx
+++ src/components/CustomDocCardSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DocCard from "@theme/DocCard";
 import { findSidebarItem } from "@site/src/sidebarUtils";
 import styles from "./CustomDocCardSection.module.css";
@@ -13,21 +13,59 @@ type Props = {
   svg: React.ReactNode;
 };
 
-export default function CustomDocCardSection({ title, description, itemPaths, fullWidth, secondDescription, secondItemPaths, svg }: Props) {
+export default function CustomDocCardSection({
+  title,
+  description,
+  itemPaths,
+  fullWidth,
+  secondDescription,
+  secondItemPaths,
+  svg,
+}: Props) {
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const [animate, setAnimate] = useState(false);
+
   const items = itemPaths.map((path) => findSidebarItem(path)).filter(Boolean);
   const secondItems = secondItemPaths?.map((path) => findSidebarItem(path)).filter(Boolean) ?? [];
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setAnimate(true);
+          observer.disconnect();
+        }
+      },
+      {
+        threshold: 0.2,
+      }
+    );
+
+    if (wrapperRef.current) {
+      observer.observe(wrapperRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <div className={`${styles.customCard} ${fullWidth ? styles.fullWidth : ""}`}>
+    <div
+      ref={wrapperRef}
+      className={`${styles.customCard} ${fullWidth ? styles.fullWidth : ""}`}
+    >
       <div className={`${fullWidth && styles.cardContentFlex}`}>
-      {svg && (
-        <div className={styles.animatedSvgWrapper}>
-      {svg}
-    </div>
-  )}
+        {svg && (
+          <div
+            className={`${styles.animatedSvgWrapper} ${
+              animate ? styles.animateSvg : ""
+            }`}
+          >
+            {svg}
+          </div>
+        )}
         <div className={`${fullWidth && styles.halfWidth}`}>
-            <h2>{title}</h2>
-            <p className={styles.cardDescription}>{description}</p>
+          <h2>{title}</h2>
+          <p className={styles.cardDescription}>{description}</p>
         </div>
 
         <div className={`${styles.cardLinksList} ${fullWidth && styles.halfWidth}`}>
@@ -38,8 +76,8 @@ export default function CustomDocCardSection({ title, description, itemPaths, fu
           ))}
         </div>
 
-          {secondDescription && <p>{secondDescription}</p>}
-        
+        {secondDescription && <p>{secondDescription}</p>}
+
         <div className={styles.cardLinksList}>
           {secondItems.length > 0 && (
             <div className={styles.cardLinksList}>
